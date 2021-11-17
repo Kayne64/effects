465cdbf6477daa42ca6809cd581b593db1ebdff9
Kayne <93083042+Kayne64@users.noreply.github.com>
2021-11-17 22:31:05 +0800

    new push
    * - After death it resets
    * - Shortened length of code after waiting

--- cl.lua
+++ cl.lua
@@ -9,34 +9,39 @@ function alert(msg)
     DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
 
--- Commands DRUNK IS WORKING
+-- Commands
 RegisterCommand("drunk", function(source, args)
     alert("~g~You just drank some booze")
-    --TaskStartScenarioInPlace(player, "WORLD_HUMAN_DRINKING", 0, 1) 
+    TaskStartScenarioInPlace(player, "WORLD_HUMAN_DRINKING", 0, 1) 
     SetPedMotionBlur(player, true)
     AnimpostfxPlay ("DrugsMichaelAliensFight", 10000001, true)
     SetPedIsDrunk(player, true)
-    SetPedMovementClipset(player, "MOVE_M@SLOW", true)
+    SetPedMovementClipset(player, "MOVE_M@DRUNK@VERYDRUNK", true)
     SetRunSprintMultiplierForPlayer(player, 1.20)
     ShakeGameplayCam("DRUNK_SHAKE", 1.0)
-    Citizen.Wait(10000)
-    SetPedMoveRateOverride(PlayerId(),1.0)
-    SetRunSprintMultiplierForPlayer(PlayerId(),1.0)
+
+    --Waiting
+    Citizen.Wait(100000)
+
+    --Resets back to Normal
+    IsPlayerDead(GetPlayerPed(-1), true) then 
+        SetPedIsDrunk(GetPlayerPed(-1), false)		
+         SetPedMotionBlur(playerPed, false)
+        ResetPedMovementClipset(GetPlayerPed(-1))
+        AnimpostfxStopAll()
+         ShakeGameplayCam("DRUNK_SHAKE", 0.0)
+end)
+
+RegisterCommand("weed", function(source, args)
+
+end)
+
+
+-- If player is dead, Reset all.
+IsPlayerDead(GetPlayerPed(-1), true) then 
     SetPedIsDrunk(GetPlayerPed(-1), false)		
      SetPedMotionBlur(playerPed, false)
     ResetPedMovementClipset(GetPlayerPed(-1))
     AnimpostfxStopAll()
      ShakeGameplayCam("DRUNK_SHAKE", 0.0)
-     SetTimecycleModifierStrength(0.0)
-end)
-
-RegisterCommand("weed", function(source, args)
-    alert("~g~You just smoked some weed.")
-TaskStartScenarioInPlace(player, "WORLD_HUMAN_SMOKING_POT", 0, 1) 
-    SetPedMotionBlur(player, true)
-    SetPedIsDrunk(player, true)
-    SetPedMovementClipset(player, "MOVE_M@SLOW", true)
-    SetRunSprintMultiplierForPlayer(player, 1.20)
-    ShakeGameplayCam("DRUNK_SHAKE", 1.0)
-    Citizen.Wait(1000)
 end)
